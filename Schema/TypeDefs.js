42130a20f0a8e50a4d35d32ae2b5d839c49549d4
realemeralds <102229990+realemeralds@users.noreply.github.com>
2022-08-17 00:28:59 +0800

    :sparkles: added basic file structure

--- Schema/TypeDefs.js
+++ Schema/TypeDefs.js
@@ -1,15 +1,47 @@
 const { gql } = require("apollo-server-express");
 
+// getCityByName(name: String!, country: String, config: ConfigInput): [City!]!;
 const typeDefs = gql`
     type Query {
-        cities: cityType;
-        forecast: forecastType;
+        getForecastByCoords(lat: String!, lon: String!): [Forecast!]!;
+    }
+
+    type Forecast {
+        dt: Int!,
+        dt_txt: String!,
+        main: ForecastDetails!,
+        weather: WeatherDetails!,
+        cloudCover: Int!,
+        wind: WindDetails!,
+        visibility: Int!,
+        chanceOfRain: Int!,
+        volumeOfRain: Int!,
+        volumeOfSnow: Int!,
+    }
+
+    type ForecastDetails {
+        temp: Int!,
+        feelsLike: Int!,
+        tempMin: Int!,
+        tempMax: Int!,
+        pressure: Int!,
+        pressureSeaLevel: Int!,
+        grndLevel: Int!,
+        humidity: Int!,
+    }
+
+    type WeatherDetails {
+        id: Int!,
+        main: String!,
+        description: String!,
+        icon: String!
+    }
+
+    type WindDetails{
+        speed: Float!,
+        deg: Int!,
+        gust: Float!,
     }
 `;
 
-/**
- * I need two types:
- * 1. For the geocaching, which I can use to go all the way
- * 2. For just the location + weather from the coordinates, which
- * I can manipulate into an array of past temperatures.
- */
+export default typeDefs;
